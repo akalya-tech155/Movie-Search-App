c2a8fe2a29a818a2d3c555d6ca5e70ae6317f7a1
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-16 18:34:33 +0530

    created new file

---
+++ script.js
@@ -0,0 +1,23 @@
+const searchBtn = document.getElementById("searchBtn");
+const movieDetails = document.getElementById("movieDetails");
+
+searchBtn.addEventListener("click", async () => {
+
+    const movieName = document.getElementById("movieName").value;
+
+    const apiKey = "YOUR_API_KEY";
+
+    const url =
+      `https://www.omdbapi.com/?t=${movieName}&apikey=${apiKey}`;
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    movieDetails.innerHTML = `
+        <h2>${data.Title}</h2>
+        <img src="${data.Poster}">
+        <p><strong>Year:</strong> ${data.Year}</p>
+        <p>${data.Plot}</p>
+    `;
+});
